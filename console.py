b4103d9e4144a9d0784beae65fc0b6e92c5b91bc
KOBOKO23 <kobokomasero@gmail.com>
2024-07-25 22:28:26 +0300

    console.py

--- console.py
+++ console.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
+
+"""
+
+"""
 import cmd
 
 class HBNBCommand(cmd.Cmd):
     """
     Command processor for the HBNB console.
     """
-    prompt = "(hbnb) "
+    prompt = "(hbnb)"
 
     def do_quit(self, arg):
         """
         Quit command to exit the program.
         """
         return True
+
+    def help_quit(self, arg):
+        """
+        Help for the quit command.
+        """
+        print("Quit command to exit the program")
 
     def do_EoF(self, arg):
         """
@@ -20,23 +30,12 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
-    def help_quit(self):
-        """
-        Help for the quit command.
-        """
-        print("Quit command to exit the program")
-
-    def help_EoF(self):
+    def do_EoF(self, arg):
         """
         Help for the End of File (EOF) command.
         """
-        print("End of File (EOF) signal to exit the program")
-
-    def emptyline(self):
-        """
-        Override the default behavior for empty lines.
-        """
-        pass
+        print()
+        return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
