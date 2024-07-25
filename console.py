b8dc617c297398c8479595b1987cc6575c157cae
KOBOKO23 <kobokomasero@gmail.com>
2024-07-25 20:37:33 +0300

    console.py

--- console.py
+++ console.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""
 
-"""
 import cmd
 
 class HBNBCommand(cmd.Cmd):
     """
+    Command processor for the HBNB console.
     """
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """
+        Quit command to exit the program.
         """
         return True
 
-    def help_quit(self, arg):
+    def help_quit(self, arg=None):
         """
+        Help for the quit command.
         """
         print("Quit command to exit the program")
 
     def do_EoF(self, arg):
         """
+        Handle End of File (EOF) signal to exit the program.
         """
         print()
         return True
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
+
